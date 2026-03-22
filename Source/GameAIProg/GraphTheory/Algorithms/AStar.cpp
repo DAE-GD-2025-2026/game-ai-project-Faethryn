5b3dfc894b1b15259b1fe764ae0b53ccc40abc23
Faethryn <97239542+Faethryn@users.noreply.github.com>
2026-03-22 12:11:47 +0100

    Implemented A* pathfinding
    Fixed issue with open and closed lists and A* now works as well

--- Source/GameAIProg/GraphTheory/Algorithms/AStar.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/AStar.cpp
@@ -21,17 +21,71 @@ std::vector<Node*>AStar::FindPath(Node* const pStartNode, Node* const pGoalNode)
 	
 	while (!open_list.empty())
 	{
-		std::vector<int> neighbourCosts{};
-		std::vector<Node*> neighbours{};
+		float currentLowestCost =  GetHeuristicCost(open_list[0], pGoalNode);
+		currentNodeID = open_list[0]->GetId();
+		
+		for (int i {0}; i < open_list.size(); ++i)
+		{
+			Node* currentNode = open_list[i];
+			
+			float cost = GetHeuristicCost(currentNode, pGoalNode);
+			
+			if (cost < currentLowestCost)
+			{
+				currentLowestCost = cost;
+				currentNodeID = currentNode->GetId();
+			}
+		}
+		
+		if (currentNodeID == pGoalNode->GetId())
+		{
+			path = closed_list;
+			
+			return path;
+		}
 		
 		for (int i{0}; i < pGraph->GetConnections().size(); i++)
 		{
 			if (pGraph->GetConnections()[i]->GetFromId() == currentNodeID)
 			{
-			    neighbours.push_back(pGraph->GetNode(pGraph->GetConnections()[i]->GetToId()).get());
-				FVector2D currentPos = pGraph->GetNode(currentNodeID)->GetPosition();
-				FVector2D OtherPos = pGraph->GetNode(pGraph->GetConnections()[i]->GetToId())->GetPosition();
+				bool isClosed = false;
 				
+				for (int j {0}; j < closed_list.size(); ++j)
+				{
+					if (pGraph->GetConnections()[i]->GetToId() == closed_list[j]->GetId())
+					{
+						isClosed = true;
+					}
+				}
+				
+				if (!isClosed)
+				{
+					Node* pCurrentNeighbour = pGraph->GetNode(pGraph->GetConnections()[i]->GetToId()).get();
+					
+					bool isInOpenList = false;
+					for (int j {0}; j < open_list.size(); ++j)
+					{
+						if (pCurrentNeighbour == open_list[j])
+						{
+							isInOpenList = true;
+						}
+					}
+					
+					if (!isInOpenList)
+					{
+						open_list.push_back(pCurrentNeighbour);
+					}
+				}
+			}
+		}
+		
+		closed_list.push_back(pGraph->GetNode(currentNodeID).get());
+		
+		for (int i {0}; i < open_list.size(); ++i)
+		{
+			if (open_list[i]->GetId() == currentNodeID)
+			{
+				open_list.erase(open_list.begin() + i);
 			}
 		}
 	}
